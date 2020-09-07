5f24ad395f438a1e2745b40686d80009de77c18f
Yesika Gomez <yesika.go@hotmail.com>
2020-09-07 15:30:35 -0500

    Creando portafolio en react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bulma/css/bulma.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import './index.css';
+
+export default class App extends React.Component {
+
+  render() {
+    return (
+      <div >
+
+      </div>
+    );
+  }
+}
